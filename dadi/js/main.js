5c147cd870bed580109be55fbafda5262445f7cc
Michele <micheleierniero@gmail.com>
2021-07-16 17:31:52 +0200

    esercizio giocatore vs CPU

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -11,3 +11,18 @@ var cpuNumber = Math.floor(Math.random() * 7);
 
 alert("Il numero assegnato al computer è " + cpuNumber);
 
+
+// Chi è il Vincitore
+
+if (playerNumber > cpuNumber) {
+    document.writeln("il vincitore è il giocatore")
+}
+
+else if (playerNumber < cpuNumber) {
+    document.writeln("il vincitore è il computer")
+}
+else if (playerNumber == cpuNumber) {
+    document.writeln("Pareggio")
+}
+    
+
