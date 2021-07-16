d0ae7900ae251543a083a16008c0135ed4ea0f92
Michele <micheleierniero@gmail.com>
2021-07-16 17:12:05 +0200

    N-giocatore e N-CPU

---
+++ dadi/js/main.js
@@ -0,0 +1,13 @@
+// Numero Giocatore casuale tra 0 e 6
+
+var playerNumber = Math.floor(Math.random() * 7);
+
+alert("Il numero assegnato al giocatore è " + playerNumber);
+
+
+// Numero CPU casuale tra 0 e 6
+
+var cpuNumber = Math.floor(Math.random() * 7);
+
+alert("Il numero assegnato al computer è " + cpuNumber);
+
